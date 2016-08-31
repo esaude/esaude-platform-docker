ffc8869ebba9868faab9ca1514e88125d4e74ce5
Pascal Brandt <psbrandt@gmail.com>
2016-08-31 11:29:18 +0200

    📦 Update created image names for deploy.

--- scripts/publish-images.sh
+++ scripts/publish-images.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-set -o xtrace
 
 # Log in to the Bintray Docker registry
-docker login -u $BINTRAY_USER -p $BINTRAY_API_KEY -e $BINTRAY_EMAIL esaude-docker-platform-docker.bintray.io
+docker login -u $BINTRAY_USER -p $BINTRAY_API_KEY esaude-docker-platform-docker.bintray.io
+
+set -o xtrace
 
 # Tag the tomcat image
-docker tag `docker images -q esaudeplatformdocker_esaude-platform-tomcat` esaude-docker-platform-docker.bintray.io/tomcat:$TRAVIS_TAG
+docker tag `docker images -q esaude_esaude-platform-tomcat` esaude-docker-platform-docker.bintray.io/tomcat:$TRAVIS_TAG
 
 # Push the tomcat image
 docker push esaude-docker-platform-docker.bintray.io/tomcat:$TRAVIS_TAG
 
 # Tag the mysql image
-docker tag `docker images -q esaudeplatformdocker_esaude-platform-mysql` esaude-docker-platform-docker.bintray.io/mysql:$TRAVIS_TAG
+docker tag `docker images -q esaude_esaude-platform-mysql` esaude-docker-platform-docker.bintray.io/mysql:$TRAVIS_TAG
 
 # Push the mysql image
 docker push esaude-docker-platform-docker.bintray.io/mysql:$TRAVIS_TAG
