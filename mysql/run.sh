e598507adf961e627b58995dc07cb8bd84c542d1
Pascal Brandt <psbrandt@gmail.com>
2016-09-02 16:23:43 +0200

    🏔 Convert to Alpine Linux.

---
+++ mysql/run.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+set -ex
+
+if [ -f "$MYSQL_DATA_DIRECTORY" ]; then
+	echo 'MySQL data directory exists'
+else
+	echo 'MySQL data directory does not exist'
+
+  echo 'Initializing database'
+	mkdir -p "$MYSQL_DATA_DIRECTORY"
+	mysql_install_db --user=root --datadir="$MYSQL_DATA_DIRECTORY" --rpm
+	echo 'Database initialized'
+
+  if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+  fi
+
+	tfile=$(mktemp)
+  if [ ! -f "$tfile" ]; then
+      return 1
+  fi
+
+  cat <<-EOF > "$tfile"
+		USE mysql;
+		FLUSH PRIVILEGES;
+		GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+		UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
+		GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+		CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;
+		GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+	EOF
+
+  /usr/bin/mysqld --user=root --bootstrap --verbose=0 < "$tfile"
+  rm -f "$tfile"
+
+	cd tmp
+	unzip esaude-platform-database.sql.zip
+	cat esaude-platform-database.sql | mysql_embedded -uroot -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE"
+	rm -f esaude-platform-database.sql.zip
+fi
+
+echo 'Starting server'
+exec /usr/bin/mysqld --user=root --console
--- tomcat/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# wait for mysql to initialise
-/usr/local/wait-for-it.sh --timeout=300 esaude-platform-mysql:3306
-
-# start tomcat
-/usr/local/tomcat/bin/catalina.sh run
