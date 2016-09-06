ed3df52ada8e6d23d96b3e898a7c62ef8f96d4a1
Pascal Brandt <psbrandt@gmail.com>
2016-09-06 10:44:38 +0200

    🌍 Fix timezone and backup script.

--- tomcat/setenv.sh
+++ tomcat/setenv.sh
@@ -1,1 +1,1 @@
-export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms256m -Xmx768m -XX:PermSize=256m -XX:MaxPermSize=512m"
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms512m -Xmx1536m -XX:PermSize=256m -XX:MaxPermSize=768m -Duser.timezone=Africa/Maputo"
