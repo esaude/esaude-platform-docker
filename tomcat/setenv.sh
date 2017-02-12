f193e44e0564b57d0bc735fcd20e75976f345900
Stélio Moiane <steliomo@gmail.com>
2017-02-13 01:50:24 +0200

    including tomcat debug-mode

--- tomcat/setenv.sh
+++ tomcat/setenv.sh
@@ -1,1 +1,5 @@
 export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms512m -Xmx1536m -XX:PermSize=256m -XX:MaxPermSize=768m -Duser.timezone=Africa/Maputo"
+
+#Allow debug
+export CATALINA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+export JPDA_OPTS="-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n"
